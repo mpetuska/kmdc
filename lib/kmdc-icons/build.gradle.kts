f2505eed3066db880d1a71a1e41650cf22546a3a
PO <pocharlebois@redock.com>
2021-12-16 13:00:39 -0500

    Add support for material-icons (#40)
    * Add support for material-icons
    
    * Based on the npm https://github.com/marella/material-icons
    * All material icons as of 1.10.4 in an enum - ie: pie_chart ->
      PieChart("pie_chart") in MDCIconOpts.MDCIconType
    * Numbers written out using Proper Case 1MP -> OneMp
    * Output is a Span with the right class (Filled, Outlined, TwoTone, etc)
    * As elsewhere in KMDC, styling has to be applied externally
    
    * MDCIcons - Specify dependance on material-icons 1.10.4^
    
    * fixup! MDCIcons - Specify dependance on material-icons 1.10.4^
    
    * kmdc-icons: forward attrs to span element
    
    Co-authored-by: Raman Gupta <rocketraman@gmail.com>

---
+++ lib/kmdc-icons/build.gradle.kts
@@ -0,0 +1,18 @@
+import de.fayard.refreshVersions.core.versionFor
+
+plugins {
+  id("plugin.library-compose")
+  id("plugin.publishing-mpp")
+}
+
+kotlin {
+  sourceSets {
+    named("jsMain") {
+      dependencies {
+        val mdcIconsVersion = versionFor("version.npm.material-icons")
+        api(project(":lib:kmdc-core"))
+        api(npm("material-icons", mdcIconsVersion))
+      }
+    }
+  }
+}
