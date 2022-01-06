36a86baa5267201e541c99f8fe858414eebceaef
Pierre-Olivier Charlebois <pocharlebois@gmail.com>
2022-01-06 17:42:20 -0500

    kmdc-menu - Remove unnecessary MenuSurfaceAnchorAttrsScope

--- kmdc/kmdc-menu-surface/src/jsMain/kotlin/MDCMenuAnchor.kt
+++ kmdc/kmdc-menu-surface/src/jsMain/kotlin/MDCMenuAnchor.kt
@@ -8,18 +8,16 @@ import org.jetbrains.compose.web.dom.ContentBuilder
 import org.jetbrains.compose.web.dom.Div
 import org.w3c.dom.HTMLDivElement
 
-public class MDCMenuSurfaceAnchorAttrsScope private constructor() : AttrsBuilder<HTMLDivElement>()
-
 @MDCDsl
 @Composable
 public fun MDCMenuSurfaceAnchor(
-  attrs: Builder<MDCMenuSurfaceAnchorAttrsScope>? = null,
+  attrs: Builder<AttrsBuilder<HTMLDivElement>>? = null,
   content: ContentBuilder<HTMLDivElement>? = null,
 ) {
   Div(
     attrs = {
       classes("mdc-menu-surface--anchor")
-      attrs?.invoke(this.unsafeCast<MDCMenuSurfaceAnchorAttrsScope>())
+      attrs?.invoke(this.unsafeCast<AttrsBuilder<HTMLDivElement>>())
     },
     content = content
   )
