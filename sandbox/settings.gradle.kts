3c50788a68b21b5bb409326144d1b4bab87df681
Martynas Petuška <martynas.petuska@outlook.com>
2022-01-16 15:31:50 +0000

    refreshVersions (#74)

--- sandbox/settings.gradle.kts
+++ sandbox/settings.gradle.kts
@@ -7,8 +7,8 @@ pluginManagement {
 }
 
 plugins {
-  id("de.fayard.refreshVersions") version "0.30.1"
-  id("com.gradle.enterprise") version "3.8"
+  id("de.fayard.refreshVersions") version "0.30.2"
+  id("com.gradle.enterprise") version "3.8.1"
 }
 
 refreshVersions {
