ff9429782cee0c86936e485bb0ee3b6fd4e9f8e4
Martynas Petuška <martynas@petuska.dev>
2022-05-27 13:19:43 +0100

    floating-label

--- kmdc/kmdc-floating-label/src/jsMain/kotlin/MDCFloatingLabel.kt
+++ kmdc/kmdc-floating-label/src/jsMain/kotlin/MDCFloatingLabel.kt
@@ -2,24 +2,12 @@ package dev.petuska.kmdc.floating.label
 
 import androidx.compose.runtime.Composable
 import dev.petuska.kmdc.core.*
-import org.jetbrains.compose.web.dom.ElementScope
 import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
-import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLSpanElement
 
 @JsModule("@material/floating-label/mdc-floating-label.scss")
 private external val Style: dynamic
-
-public enum class MDCButtonType(public vararg val classes: String) {
-  Text, Outlined("mdc-button--outlined"), Raised("mdc-button--raised"), Unelevated("mdc-button--unelevated")
-}
-
-public enum class MDCButtonIconType(public vararg val classes: String) {
-  None, Leading("mdc-button--icon-leading"), Trailing("mdc-button--icon-trailing")
-}
-
-public interface MDCButtonScope : ElementScope<HTMLButtonElement>
 
 /**
  * [JS API](https://github.com/material-components/material-components-web/tree/v14.0.0/packages/mdc-floating-label)
