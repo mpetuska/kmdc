4fe47337fb4e80879ec7691596470ee7439d8c73
Martynas Petuška <martynas@petuska.dev>
2022-05-27 12:05:22 +0100

    dialog

--- kmdc/kmdc-dialog/src/jsMain/kotlin/_module.kt
+++ kmdc/kmdc-dialog/src/jsMain/kotlin/_module.kt
@@ -4,16 +4,17 @@ package dev.petuska.kmdc.dialog
 
 import dev.petuska.kmdc.core.MDCComponent
 import dev.petuska.kmdc.core.MDCExternalAPI
+import dev.petuska.kmdc.core.MDCLayoutComponent
 import org.w3c.dom.Element
 
 @MDCExternalAPI
-public external class MDCDialog(element: Element) : MDCComponent<dynamic> {
+public external class MDCDialog(element: Element) : MDCComponent<dynamic>, MDCLayoutComponent {
   public val isOpen: Boolean
   public var escapeKeyAction: String
   public var scrimClickAction: String
   public var autoStackButtons: Boolean
 
-  public fun layout()
+  public override fun layout()
   public fun open()
   public fun close(action: String = definedExternally)
 }
